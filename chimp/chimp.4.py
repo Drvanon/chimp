1e16808cc583b38fe53f3e38fd8d705ede066402
drvanon <robbiedorsrtijn@gmail.com>
2012-01-13 20:25:14 +0100

    FINISHED THE HIGHSCORE MODULE!!! (and started cleaning up the chimp.4 file)

--- chimp/chimp.4.py
+++ chimp/chimp.4.py
@@ -1,6 +1,7 @@
 import os, pygame, sys, time
 from pygame.locals import *
 import highscore
+from Sprites import *
 
 if not pygame.font: print 'Warning, fonts disabled'
 if not pygame.mixer: print 'Warning, sound disabled'
@@ -8,7 +9,6 @@ if not pygame.mixer: print 'Warning, sound disabled'
 height = 600
 width = 600
 
-added = 0 
 
 def load_image(name, colorkey=None):
     fullname = os.path.join('data', name)
@@ -38,144 +38,6 @@ def load_sound(name):
         raise SystemExit, message 
     return sound
 
-class Fist(pygame.sprite.Sprite):    
-    def __init__(self):
-        pygame.sprite.Sprite.__init__(self) 
-        self.image, self.rect = load_image('fist.png', -1)
-        self.punching = 0
-        self.TimesPunched = 0
-        
-    def update(self):
-        pos = pygame.mouse.get_pos()
-        self.rect.midtop = pos
-        if self.punching:
-            self.rect.move_ip(5, 10)
-            
-    def punch(self, target):
-        if not self.punching:
-            self.punching = 1
-            hitbox = self.rect.inflate(-5, -5)
-            self.TimesPunched = self.TimesPunched + 1
-            return hitbox.colliderect(target.rect)
-            return TimesPunched
-            
-    def unpunch(self):
-        self.punching = 0
-                                      
-class Bomb(pygame.sprite.Sprite):
-    def __init__(self):
-        pygame.sprite.Sprite.__init__(self)
-        self.image, self.rect = load_image('bomb.png', -1)
-        screen = pygame.display.get_surface()
-        self.area = screen.get_rect()
-        self.rect.topleft = 400, 60
-        self.move = [4, 5]
-        self.hit = 0
-        self.colliderate = 0
-           
-    def _collider(self, target):        
-        hitbox = self.rect.inflate(-5, -5)
-        return hitbox.colliderect(target.rect)
-        
-        
-    def update(self):
-        if self.hit:
-            self._kaboom()
-        else:
-            self._move()
-            
-    def _move(self):
-        newpos = self.rect.move((self.move[0], self.move[1]))
-        if self.rect.left < self.area.left or \
-           self.rect.right > self.area.right:
-            self.move[0] = -self.move[0]
-            newpos = self.rect.move((self.move))            
-        self.rect = newpos
-        if self.rect.top < 0 or self.rect.bottom > height:
-            self.move[1] = -self.move[1]
-            
-    def _kaboom(self):                                  
-        self.colliderate = 1                                        
-        pygame.mouse.set_visible(1) 
-                       
-class Chimp(pygame.sprite.Sprite):
-    def __init__(self):
-        pygame.sprite.Sprite.__init__(self)
-        self.image, self.rect = load_image('chimp.png', -1)
-        screen = pygame.display.get_surface()
-        self.area = screen.get_rect()
-        self.rect.topleft = 10, 10
-        self.move = 9
-        self.dizzy = 0
-        self.TimesHit = 0
-        
-    def update(self):
-        if self.dizzy:
-            self._spin()
-        else:
-            self._walk()
-            
-    def _walk(self):
-        newpos = self.rect.move((self.move, 0))
-        if self.rect.left < self.area.left or \
-           self.rect.right > self.area.right:
-            self.move = -self.move
-            newpos = self.rect.move((self.move, 0))
-            self.image = pygame.transform.flip(self.image, 1, 0)
-        self.rect = newpos
-        
-    def _spin(self):
-        center = self.rect.center
-        self.dizzy = self.dizzy + 12
-        if self.dizzy >= 360:
-            self.dizzy = 0
-            self.image = self.original
-        else:
-            rotate = pygame.transform.rotate
-            self.image = rotate(self.original, self.dizzy)
-        self.rect = self.image.get_rect(center=center)
-        
-    def punched(self):
-        if not self.dizzy:
-            self.dizzy = 1
-            self.original = self.image
-            self.TimesHit = self.TimesHit + 1
-               
-
-class Game_over(pygame.sprite.Sprite):
-    def __init__(self):
-        pygame.sprite.Sprite.__init__(self)
-        self.image, self.rect = load_image('game_over.png', -1)
-        screen = pygame.display.get_surface()
-        self.area = screen.get_rect()
-        self.rect.topleft = 0, 0
-
-class MainMenu(pygame.sprite.Sprite):
-    def __init__(self):
-        pygame.sprite.Sprite.__init__(self)
-        self.start_game = 0
-        
-    def _ButtonClicked(self):
-        self.start_game = 1
-        
-        
-class Button(pygame.sprite.Sprite):
-    def __init__(self):
-        pygame.sprite.Sprite.__init__(self)
-        self.image, self.rect = load_image('button.png', -1)
-        screen = pygame.display.get_surface()
-        self.area = screen.get_rect()
-        self.rect.topleft = 160, 0
-        self.a = 0
-    
-    def update(self):
-        self._click()
-        
-    def _click(self):
-        hitbox = self.rect.inflate(-5, -5)
-        pos = pygame.mouse.get_pos()
-        if hitbox.collidepoint(pos):
-            self.a = 1      
 
 TimesHit = 0
     
@@ -187,6 +49,8 @@ def main():
     background = pygame.Surface(screen.get_size())
     background = background.convert()
     background.fill((88, 87, 70))
+    
+    added = 0 
     
     chimp = Chimp()
     fist = Fist()
