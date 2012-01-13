19d44e687476ddf5f46200a17a95b48b99cf4bab
drvanon <robbiedorsrtijn@gmail.com>
2012-01-13 20:13:15 +0100

    trying but failing to solve the just one time problem

--- chimp/highscore.py
+++ chimp/highscore.py
@@ -1,29 +1,18 @@
-def print_all(f):
-    print f.read()
-    
 def rewind(f):
     f.seek(0)
 
 def print_a_line(name, score):
-    higscore = open('highscores.txt', 'w')
+    higscore = open('highscores.txt', 'a')
     score = str(score)
     higscore.write(score)
     higscore.write('\t')
     higscore.write(name)
+    higscore.write('\n')
 
 def DefineFile():    
     global current_file
     current_file = open('highscores.txt', 'w')
-
-def TimesPlayed(timesplayed = 0):
-    global current_line
-    current_line = timesplayed
     
 def historic(score, name):
-    TimesPlayed()
     rewind(current_file)
     print_a_line(name, score)
-    
-def newline():
-    h = open('highscore.txt', 'w')
-    h.write('\n')
